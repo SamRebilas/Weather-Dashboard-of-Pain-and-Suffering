3de209b5d700909e388d1e7ac9cb6fd8fb1ce520
Samrebilas <sam.rebilas@gmail.com>
2022-07-11 11:08:48 -0400

    finally api is console logging

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,15 +1,8 @@
-var button = document.querySelector('.button')
-var inputValue = document.querySelector('.search')
-var name = document.querySelector('.cityName')
-var temp = document.querySelector('.temp')
-var wind = document.querySelector('.wind')
-var humidity = document.querySelector('.humidity')
-var uvIndex = document.querySelector('.uv-index')
+var apiKey =  "2eaaf63cfb416bf2e134c49946bd1b16"
 
-button.addEventListener('click', function(){
-    fetch('https://api.openweathermap.org/data/2.5/weather?lat='+inputValue.value+'&lon='+inputValue.value+'&appid=6a95f043ca0f7aaa2c813fca9771dc41')
-    .then(response => response.json())
-    .then(data => console.log(data))
-    
-    .catch(err => alert("wrong city name"))
-})
+function weather (){
+    fetch("https://api.openweathermap.org/data/2.5/weather?q=Austin&appid=2eaaf63cfb416bf2e134c49946bd1b16"
+    ).then((response) => response.json())
+    .then((data) => console.log(data))
+}
+weather()
