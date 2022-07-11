fceee37f79a9f0fd05707640da2e4ba1549c28b0
Samrebilas <sam.rebilas@gmail.com>
2022-07-11 12:46:17 -0400

    js display weather function is working, fixed css to diplay in propper sections of screen and used moment to show current date

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,8 +1,36 @@
-var apiKey =  "2eaaf63cfb416bf2e134c49946bd1b16"
+const apiKey =  "2eaaf63cfb416bf2e134c49946bd1b16"
 
-function weather (){
-    fetch("https://api.openweathermap.org/data/2.5/weather?q=Austin&appid=2eaaf63cfb416bf2e134c49946bd1b16"
-    ).then((response) => response.json())
-    .then((data) => console.log(data))
+
+$(document).ready(function(){
+    $("#date").text( moment().format('ddd MMM Do, YYYY'))
 }
-weather()
+)
+
+function weather (city){
+    fetch("https://api.openweathermap.org/data/2.5/weather?q=" 
+    + city +"&appid=" + apiKey
+    )
+    .then((response) => response.json())
+    .then((data) => this.displayWeather(data))
+}
+function displayWeather (data) {
+const {name} = data
+document.querySelector(".city").innerHTML = name
+const {temp} = data.main
+document.querySelector(".temp").innerHTML = "Temperature: " + temp
+const { speed } = data.wind
+document.querySelector(".wind").innerHTML = "Wind: " + speed
+const {humidity} = data.main
+document.querySelector(".humidity").innerHTML = "Humidity: " + humidity 
+}
+{
+    function search(){
+        this.weather(document.querySelector(".search-bar").value)
+    }
+}
+
+
+
+document.querySelector(".btn").addEventListener("click", function(){
+    search()
+})
